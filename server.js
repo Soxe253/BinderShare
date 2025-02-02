5247c16688993e014aa2f92b6e5beac245debd7f
Spencer <soxe@pugetsound.edu>
2025-02-02 14:25:40 -0800

    add and delete cards

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import mongoose from 'mongoose';
 import Card from './models/card.js';
+import { filterOptions } from 'mongodb/lib/utils.js';
 
 const app = express();
 app.use('/public', express.static('public'));
@@ -23,7 +24,35 @@ app.get('/cards', async (req,res) => {
     }
 });
 
-//add card
+//add card- real function
+app.post('/cards', async (req, res) => {
+    const {name, cardNumber, auto, variant } = req.body;
+    try{
+        const card = new Card({name, cardNumber, auto, variant});
+        await card.save();
+        res.status(201).send(card);
+    } catch(e) {
+        res.status(400).send(e.message);
+    }
+});
+
+//delete card- real function
+app.delete('/cards', async (req, res) => {
+    const {name, cardNumber, auto, variant } = req.body;
+    try{
+        const card = Card.findOneAndDelete({name, cardNumber, auto, variant });
+
+        if(!card){
+            return res.status(404).send('Card not found');
+        }
+        
+        res.status(200).send('Card deleted successfully');
+    }catch (e) {
+        res.status(500).send(e.message);
+    }
+   
+});
+//add card- test function
 async function addCard(){
     try{
         const card = new Card ({
